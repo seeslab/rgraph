a215f41d9d703d126805caf1fee05c2422d69e83
Roger Guimerà <roger.guimera@urv.cat>
2007-11-28 23:57:15 +0000

    A test added for the tools library.

---
+++ test/main_tools.c
@@ -0,0 +1,46 @@
+/*
+  main_tools.c
+  $LastChangedDate: 2007-06-26 18:15:39 -0500 (Tue, 26 Jun 2007) $
+  $Revision: 60 $
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#include "tools.h"
+
+#define EPS 1.e-8
+
+int main()
+{
+  int result = 0;
+  double *vec = allocate_d_vec(4);
+  double m, s, mi, ma;
+
+  vec[0] = 1.0;
+  vec[1] = 1.0;
+  vec[2] = 2.0;
+  vec[3] = 4.0;
+  
+  m = mean(vec, 4);
+  s = stddev(vec, 4);
+  mi = min(vec, 4);
+  ma = max(vec, 4);
+
+  if (fabs(m - 2.0) > EPS)
+    result = 1;
+  if (fabs(s - sqrt(3.0 / 2.0)) > EPS)
+    result = 1;
+  if (fabs(mi - 1.0) > EPS)
+    result = 1;
+  if (fabs(ma - 4.0) > EPS)
+    result = 1;
+
+  fprintf(stdout, "mean=%lf stddev=%lf min=%lf max=%lf\n",
+	  m, s, mi, ma);
+
+  free_d_vec(vec);
+
+  return result;
+}
