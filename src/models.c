ccead28ac4c8a52506d04d77783fd343fb522ecf
Roger Guimerà <roger.guimera@urv.cat>
2009-01-27 20:26:13 +0000

    Added EmptyGraph function.

--- src/models.c
+++ src/models.c
@@ -13,9 +13,36 @@
 #include "graph.h"
 #include "models.h"
 
-// ---------------------------------------------------------------------
-// Create an Erdos-Renyi random graph
-// ---------------------------------------------------------------------
+/*
+  ---------------------------------------------------------------------
+  Create an empty graph
+  ---------------------------------------------------------------------
+*/
+struct node_gra *
+EmptyGraph(int S)
+{
+  int node1;
+  struct node_gra *root = NULL, *last = NULL;
+  char label[MAX_LABEL_LENGTH];
+
+  /* Create header */
+  last = root = CreateHeaderGraph();
+  
+  /* Create nodes */
+  for (node1=0; node1<S; node1++) {
+    sprintf(&label[0], "%d", node1+1);
+    last = CreateNodeGraph(last, &label[0]);
+  }
+
+  /* Done */
+  return root;
+}
+
+/*
+  ---------------------------------------------------------------------
+  Create an Erdos-Renyi random graph
+  ---------------------------------------------------------------------
+*/
 struct node_gra *
 ERGraph(int S, double p, struct prng *gen)
 {
@@ -24,17 +51,17 @@ ERGraph(int S, double p, struct prng *gen)
   struct node_gra *root = NULL, *last = NULL;
   char label[MAX_LABEL_LENGTH];
 
-  // Create header
+  /* Create header */
   last = root = CreateHeaderGraph();
   
-  // Create nodes
+  /* Create nodes */
   nodeList = (struct node_gra **)calloc(S, sizeof(struct node_gra *));
   for (node1=0; node1<S; node1++) {
     sprintf(&label[0], "%d", node1+1);
     last = nodeList[node1] = CreateNodeGraph(last, &label[0]);
   }
 
-  // Create the links
+  /* Create the links */
   for (node1=0; node1<S; node1++) {
     for (node2=node1+1; node2<S; node2++) {
       if (prng_get_next(gen) < p) {
@@ -44,7 +71,7 @@ ERGraph(int S, double p, struct prng *gen)
     }
   }
 
-  // Done
+  /* Done */
   free(nodeList);
   return root;
 }
--- src/models.h
+++ src/models.h
@@ -9,6 +9,7 @@
 
 #include "prng.h"
 
+struct node_gra *EmptyGraph(int S);
 struct node_gra *ERGraph(int S, double p, struct prng *gen);
 
 #endif /* !RGRAPH_MODELS_H */
