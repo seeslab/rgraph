6c6734c06f602de0d11948e52648f638a06ccf8c
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2015-03-11 17:26:33 +1300

    bipartmod_w_cl now fallback to interactive mode when no arguments are provided.

--- bipartmod/main_bipartmod_w_cl.c
+++ bipartmod/main_bipartmod_w_cl.c
@@ -22,6 +22,8 @@ main(int argc, char **argv)
   double Ti, Tf;
   double Ts = 0.97;
   double fac = 1.0;
+  char fn_array[256];
+  char fno_array[256];
   char *file_name;
   char *file_name_out;
   int invert = 0;
@@ -38,52 +40,78 @@ main(int argc, char **argv)
   */
   
   if (argc == 1) {
-    printf("\nUsage: bipartmod_cl [-f file] [-o file] [-s seed] [-i iter] [-c cool] [-pw]\n\n"
-		   "\t -f file: Name of the input network file (default: standard input)\n"
-		   "\t -o file: Name of the output file (default: standard output)\n"
-		   "\t -s seed: Random number seed (POSITIVE Integer, default 1111) \n"
-		   "\t -i iter: Iteration factor (recommended 1.0, default 1.0)\n"
-		   "\t -c cool: Cooling factor (recommended 0.950-0.995, default 0.97)\n "
-		   "\t -p : Find modules for the second column (default: first) \n"
-		   "\t -w : Use the third column of the input file and the weighted version of the algorithm\n");
+	from_file = 1;
+	to_file = 1;	
+	file_name = fn_array;
+	file_name_out = fno_array;
 	
-    return -1;
-  }
-  else{	
-  while ((c = getopt(argc, argv, "wpf:s:i:c:o:")) != -1)
-	switch (c) {
-	case 'f':
-	  from_file = 1;
-	  file_name = optarg;
-	  //printf("input set to %s \n", file_name);
-	  break;
-	case 's':
-	  seed = atoi(optarg);
-	  //printf("seed set to %d \n", seed);
-	  break;
-	case 'i':
-	  fac = atof(optarg);
-	  //printf("iteration factor set to %f \n", fac);
-	  break;
-	case 'c':
-	  Ts = atof(optarg);
-	  //printf("cooling factor set to %f. \n", Ts);
-	  break;
-	case 'p':
-	  invert = 1;
-	  //printf("Looking at second column.\n");
-	  break;
-	case 'o':
-	  to_file = 1;
-	  file_name_out = optarg;
-	  //printf("Output file set to %s \n",file_name_out);
-	  break;
-	case 'w':
-	  weighted = 1;
-	  break;
-	}
-  }
+	printf("\n# Enter random number seed (POSITIVE integer): ");
+	scanf("%d", &seed);
+	printf("\n# Enter the name of the network file: ");
+	scanf("%s", &fn_array);
 
+	printf("\n# Enter the name of the output file: ");
+	scanf("%s", &fno_array);
+
+	printf("\n# Enter iteration factor (recommended 1.0): ");
+	scanf("%lf", &fac);
+  
+	printf("\n# Enter the cooling factor (recommended 0.950-0.995): ");
+	scanf("%lf", &Ts);
+
+	printf("\n# Find modules from first column (0) or second column (1): ");
+	scanf("%d", &invert);
+
+	printf("\n# Use the weighted (0) or weighted (1) modularity. (Edge weight is extracted from the third column): ");
+	scanf("%d", &weighted);
+  }
+  else{
+	while ((c = getopt(argc, argv, "hwpf:s:i:c:o:")) != -1)
+	  switch (c) {
+	  case 'h':
+		printf("\nUsage: bipartmod_cl [-f file] [-o file] [-s seed] [-i iter] [-c cool] [-pw]\n"
+			   "\nIf no arguments are provided, the program will fallback in interactive mode.\n\n"
+			   "\t -f file: Name of the input network file (default: standard input)\n"
+			   "\t -o file: Name of the output file (default: standard output)\n"
+			   "\t -s seed: Random number seed (POSITIVE Integer, default 1111) \n"
+			   "\t -i iter: Iteration factor (recommended 1.0, default 1.0)\n"
+			   "\t -c cool: Cooling factor (recommended 0.950-0.995, default 0.97)\n "
+			   "\t -p : Find modules for the second column (default: first) \n"
+			   "\t -w : Read edge weights from the input's third column and uses the weighted modularity.\n"
+			   "\t -h : Display this message\n");
+		return -1;
+		break;
+	  case 'f':
+		from_file = 1;
+		file_name = optarg;
+		//printf("input set to %s \n", file_name);
+		break;
+	  case 's':
+		seed = atoi(optarg);
+		//printf("seed set to %d \n", seed);
+		break;
+	  case 'i':
+		fac = atof(optarg);
+		//printf("iteration factor set to %f \n", fac);
+		break;
+	  case 'c':
+		Ts = atof(optarg);
+		//printf("cooling factor set to %f. \n", Ts);
+		break;
+	  case 'p':
+		invert = 1;
+		//printf("Looking at second column.\n");
+		break;
+	  case 'o':
+		to_file = 1;
+		file_name_out = optarg;
+		//printf("Output file set to %s \n",file_name_out);
+		break;
+	  case 'w':
+		weighted = 1;
+		break;
+	  }
+  }
 
   /*
     ------------------------------------------------------------------
@@ -105,7 +133,7 @@ main(int argc, char **argv)
   }
   else
 	binet = FBuildNetworkBipart(stdin, weighted, 0);
-
+  
   if (invert == 1)
     InvertBipart(binet);
 
